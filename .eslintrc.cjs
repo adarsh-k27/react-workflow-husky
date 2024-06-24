4c94db3ffd6e2100d3a075ec9604b927a251d741
adarsh <adarshradhakrishna@bitcot.com>
2024-06-24 04:46:38 -0700

    added changes

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs','vite.config.ts','husky'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh','react','@typescript-eslint','prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier':"error" 
+  },
+}
